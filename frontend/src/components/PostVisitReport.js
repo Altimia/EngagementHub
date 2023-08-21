8bc623ce2807cfcfa961bb43e9e67efde15cff9b
YohanesFS <yohanesfelix@hotmail.com>
2023-08-21 19:13:44 +0800

    Completed `VisitRegistration` and `PostVisitReport` components by adding form fields and handling form submission.

--- frontend/src/components/PostVisitReport.js
+++ frontend/src/components/PostVisitReport.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class PostVisitReport extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visit: '',
+      report_date: '',
+      report_details: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Handle form submission here
+  }
+
   render() {
     return (
       <div>
-        {/* Implementation of PostVisitReport component goes here */}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Visit:
+            <input type="text" value={this.state.visit} onChange={e => this.setState({ visit: e.target.value })} />
+          </label>
+          <label>
+            Report Date:
+            <input type="date" value={this.state.report_date} onChange={e => this.setState({ report_date: e.target.value })} />
+          </label>
+          <label>
+            Report Details:
+            <textarea value={this.state.report_details} onChange={e => this.setState({ report_details: e.target.value })} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
--- frontend/src/components/VisitRegistration.js
+++ frontend/src/components/VisitRegistration.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class VisitRegistration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      community: '',
+      visitor_name: '',
+      visit_date: '',
+      visit_details: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Handle form submission here
+  }
+
   render() {
     return (
       <div>
-        {/* Implementation of VisitRegistration component goes here */}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Community:
+            <input type="text" value={this.state.community} onChange={e => this.setState({ community: e.target.value })} />
+          </label>
+          <label>
+            Visitor Name:
+            <input type="text" value={this.state.visitor_name} onChange={e => this.setState({ visitor_name: e.target.value })} />
+          </label>
+          <label>
+            Visit Date:
+            <input type="date" value={this.state.visit_date} onChange={e => this.setState({ visit_date: e.target.value })} />
+          </label>
+          <label>
+            Visit Details:
+            <textarea value={this.state.visit_details} onChange={e => this.setState({ visit_details: e.target.value })} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
